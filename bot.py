6fb87a5d8631b6d5cffe2a4632f32f7f128a06b6
ardan-dev <53343651+ardan-dev@users.noreply.github.com>
2019-07-26 23:21:26 +0900

    Update bot.py

--- bot.py
+++ bot.py
@@ -5,7 +5,7 @@ from discord.ext import commands
 
 #Servers {}
 
-TOKEN = 'NTQzNDI4OTk5NDE4ODcxODI4.XTsK1A.ilHmxmX_BSpZ4w92SatJFMqI3XI'
+TOKEN = 'NjA0MTg4MDEzMTM5OTg0Mzg0.XTsMUQ.VhfwYs6yiv_o0e1tZ0ZLRmIKr1o'
 
 client = commands.Bot(command_prefix = '>>')
 
