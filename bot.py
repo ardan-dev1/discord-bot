159d1e5851ddcaef193e875efa280846fa08c548
ardan-dev <53343651+ardan-dev@users.noreply.github.com>
2019-07-28 18:29:58 +0900

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,18 +1,24 @@
 import discord
-import json
-import os
 from discord.ext import commands
 
-#Servers {}
-
-TOKEN = 'NjA0MTg4MDEzMTM5OTg0Mzg0.XT0i9g.TakOhWcCoKy0zPOSKZNinaaZ-d8'
-
-client = commands.Bot(command_prefix = '>>')
+client = commands.Bot(command_prefix = '*')
 
 @client.event
 async def on_ready():
-    print('test')
-    await client.change.presence(game=discord.Game(name='No Direct Message', type=0))
+    print('bot')
+    await client.change_presence(activity=discord.Streaming(name="GameForum | {} Users".format(len(client.users)), url='https://www.twitch.tv/ardankjr'))
+
+@client.command()
+async def clear(ctx, amount=5):
+    await ctx.channel.purge(limit=amount)
+
+@client.command()
+async def kick(ctx, member : discord.Member, *, reason=None):
+    await member.kick(reason=reason)
+
+@client.command()
+async def ban(ctx, member : discord.Member, *, reason=None):
+    await member.ban(reason=reason)
 
 
-client.run(TOKEN)
+client.run('NjA0MTg4MDEzMTM5OTg0Mzg0.XT1e1g.xLdHVfg7Tb3YQeJfsFACsACxEnY')
